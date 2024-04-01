15bc22616f7019892bffeff6bf341595e331cf82
Ivan Labiak <71697480+ILabiak@users.noreply.github.com>
2024-04-01 20:44:49 +0300

    add link icons for Foundations components

--- client/components/DonationsInfo/FoundationInfo.jsx
+++ client/components/DonationsInfo/FoundationInfo.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect, use } from 'react';
 import styles from './donationsinfo.module.css';
-import { Button, Typography, Box, Grid, IconButton, Paper, Tab } from '@mui/material';
+import { Button, Typography, Box, Link, IconButton, Paper, Tab } from '@mui/material';
 import CircularProgress from '@mui/material/CircularProgress';
 import ContentCopyOutlinedIcon from '@mui/icons-material/ContentCopyOutlined';
+import LaunchIcon from '@mui/icons-material/Launch';
 import Image from 'next/image';
 
 
@@ -12,23 +13,33 @@ function FoundationInfo({ foundation, copyData }) {
         <Box key={foundation.orgName}>
             <Box className={styles.foundation}>
                 <Box className={styles.foundationInfo}>
-                    <Box className={styles.imageWrap}>
-                        <Image alt={foundation.orgName} src={foundation.orgLogo} width={120} height={0} style={{
-                            height: '100%',
-                            maxHeight: '90px'
-                        }}></Image>
+                    <Box className={styles.foundationName}>
+                        <Box className={styles.imageWrap}>
+                            <Image alt={foundation.orgName} src={foundation.orgLogo} width={120} height={0} style={{
+                                height: '100%',
+                                maxHeight: '90px'
+                            }}></Image>
+                        </Box>
+                        <Typography sx={{
+                            ml: '20px',
+                            fontFamily: 'DM Sans',
+                            fontSize: '20px',
+                            fontWeight: '900'
+                        }}>
+                            {foundation.orgName}
+                        </Typography>
                     </Box>
-                    <Typography sx={{
-                        ml: '20px',
-                        fontFamily: 'DM Sans',
-                        fontSize: '20px',
-                        fontWeight: '900'
-                    }}>
-                        {foundation.orgName}
-                    </Typography>
+                    <Link sx={{
+                        color: '#A3A3A3',
+                        marginRight: '10px'
+                    }}
+                    href={foundation.link} target="_blank">
+                        <LaunchIcon />
+                    </Link>
+
                 </Box>
                 <Box className={styles.foundationWallets}>
-                    {foundation.addresses.map((el,index) => (
+                    {foundation.addresses.map((el, index) => (
                         <Box className={styles.walletWrap} key={index}>
                             <Typography sx={{
                                 fontFamily: 'DM Sans',
